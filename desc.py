d4c7c7fd07650f6cdcf6116c95bdda46f9d41ed2
Haliava <grenka388@gmail.com>
2019-08-14 10:53:26 +0400

    added databases and different chats

--- desc.py
+++ desc.py
@@ -3,13 +3,15 @@ import yandex_dictionary
 import pyaspeller
 import re
 import pymorphy2
-from grammarBot.cases import do_a_thing
+from cases import do_a_thing
 from telethon import TelegramClient, events
 
 dictionary = yandex_dictionary.YandexDictionary('dict.1.1.20190810T071133Z.93e0d48371a4a683.'
                                                 'c63043807bfed4b9f65746168850f4f8d6de0d35')
 api_id = 923499
-group_mode = False
+chats = [-270467580]
+chats_group_modes = {x: False for x in chats}
+print(chats_group_modes)
 api_hash = 'd01f94c8d80d5ce066b20ca0cd59dd7a'
 bot_token = '885032569:AAGOUfwyUIMZjCveJLRtHi5x8xpCk9Bj2F8'
 inv_link = 'https://t.me/joinchat/KR-2gxSdvFh3hSHnedSjag'
@@ -18,51 +20,51 @@ client = TelegramClient(bot_token, api_id, api_hash)
 morph = pymorphy2.MorphAnalyzer()
 client.start()
 
-# TODO получить все чаты бота
-@client.on(events.NewMessage(chats=['Тестируем бота вместе']))
+
+@client.on(events.NewMessage(chats=chats_group_modes.keys))
 def get_and_send(event):
     log(event.message)
     client.send_message(bot_token, event.message)
 
 
-dials = client.get_dialogs(entity=client.get_entity('KubitProBot'))
-print(dials)
+def check_chats(mess):
+    if mess.chat.id not in chats:
+        chats.append(mess.chat.id)
+        chats_group_modes[mess.chat.id] = False
 
 
 @bot.message_handler(commands=['start', 'че'])
 def start_message(message):
+    check_chats(message)
     bot.send_message(message.chat.id, 'Привет, для проверки орфографии одного слова или предложения'
                                       ' просто напиши его.\n\n'
                                       'Для подбора синонимов к слову напиши "синоним (слово в любом падеже)"\n\n'
                                       'Для склонения слова напиши "склонение (слово в любом падеже)"')
 
 
-@bot.message_handler(commands=['stop'])
-def stop_the_bot(message):
-    bot.send_message(message.chat.id, 'Выключачюсь...')
-    bot.stop_polling()
-
-
 @bot.message_handler(commands=['switch_group_mode'])
 def switch_group_mode(message):
-    global group_mode
-    if group_mode:
+    check_chats(message)
+    if chats_group_modes[message.chat.id]:
         bot.send_message(message.chat.id, 'Режим групп выключен')
-        group_mode = False
-    else:
+        chats_group_modes[message.chat.id] = False
+    elif not chats_group_modes[message.chat.id]:
         bot.send_message(message.chat.id, 'Режим групп включен')
-        group_mode = True
+        chats_group_modes[message.chat.id] = True
+    print(chats_group_modes)
 
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_id(message):
-    if not group_mode:
+    check_chats(message)
+    if not chats_group_modes[message.chat.id]:
         bot.send_message(message.chat.id, f'id стикера: {str(message.sticker.file_id)}')
     print(message.sticker.file_id)
 
 
 @bot.message_handler(func=lambda m: re.match(r'(склонение)[. ]*\w+', m.text.lower()), content_types=['text'])
 def reaction(message):
+    check_chats(message.chat)
     log(message)
     word = message.text.split()[1]
     cased_word = do_a_thing(word)
@@ -74,6 +76,7 @@ def reaction(message):
                      not re.match(r'^(синоним|склонение)', m.text.lower()), content_types=['text'])
 def correct_sentence(message):
     log(message)
+    check_chats(message)
     words = message.text.split()
     right_version = []
     mistake = False
@@ -91,13 +94,14 @@ def correct_sentence(message):
             bot.send_message(message.chat.id, f'Вы написали предложение не совсем правильно.\n\n'
                                               f'Вот исправленная версия: {" ".join([x for x in right_version])}')
         else:
-            if not group_mode:
+            if not chats_group_modes[message.chat.id]:
                 bot.send_message(message.chat.id, 'Всё верно!')
 
 
 @bot.message_handler(func=lambda m: re.fullmatch(r'\w+[^\.]', m.text), content_types=['text'])
 def correct_sentence(message):
     log(message)
+    check_chats(message)
     if not pyaspeller.Word(message.text).correct:
         if pyaspeller.Word(message.text).spellsafe is not None:
             bot.send_message(message.chat.id, f'Вы написали слово "{message.text}" неправильно.\n'
@@ -105,13 +109,14 @@ def correct_sentence(message):
         else:
             bot.send_message(message.chat.id, 'Чё')
     else:
-        if not group_mode:
+        if not chats_group_modes[message.chat.id]:
             bot.send_message(message.chat.id, 'Слово написано правильно')
             bot.send_sticker(message.chat.id, 'CAADAgADGwADTfnCECoqo6RQnjYkFgQ')
 
 
 @bot.message_handler(func=lambda m: re.match(r'(синоним)[. ]*\w+', m.text.lower()), content_types=['text'])
 def the_process(message):
+    check_chats(message)
     log(message)
     word = message.text.split()[1]
     variants = dictionary.lookup(word, 'ru', 'ru')
@@ -129,11 +134,13 @@ def the_process(message):
 
 @bot.message_handler(regexp=r'(((http[s]?):\/\/?)|^\w+\.)(\w+\.)+\w+\/')
 def react_to_link(message):
+    check_chats(message)
     bot.send_message(message.chat.id, 'Подозрительная ссылка...')
 
 
 @bot.message_handler(func=lambda m: '/t.me/joinchat/' in m.text)
 def reject(message):
+    check_chats(message)
     bot.send_message(message.chat.id, 'Не-а')
 
 
