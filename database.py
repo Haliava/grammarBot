fc3c608e88c8eec3907394c3da54d1d52029204a
Haliava <grenka388@gmail.com>
2019-08-16 14:25:15 +0400

    new task

--- database.py
+++ database.py
@@ -1,8 +1,18 @@
-import sqlite3
-import desc
+import pyautogui
+import webbrowser
+import bs4
+import requests
+import selenium
 
-db = sqlite3.connect('conversations.db')
-cursor = db.cursor()
-
-cursor.execute('''CREATE TABLE message
-               (message text)''')
+# TODO pyautogui.typewrite(word, interval=.1)
+# TODO pyautogui.hotkey('ctrl', 'shift', 'w')
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+chosen_words = ['iphone']
+url = f'https://www.google.com.ua/search?q={chosen_words[0]}'
+res = requests.get(url)
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+ads = soup.find_all(class_='ads-ad')
+print(ads)
+#webbrowser.get(chrome_path).open(url)
+#screenWidth, screenHeight = pyautogui.size()
+#pyautogui.hotkey('alt', 'left')
